be4da3a93a0ab28b4c7194ca5305c3d2f4383291
Vasuki Sunder <vasukisunder@Vasukis-MBP.attlocal.net>
2020-08-26 10:38:41 -0700

    setting up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <p>hello</p>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById("root"));
